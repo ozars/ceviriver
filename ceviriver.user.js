7e94f0bef55915246f8ce97077bd3b1866da6b57
Omer Ozarslan <code@ozarslan.com>
2018-08-18 13:09:54 -0500

    Initial commit

---
+++ ceviriver.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name            CeviriVer
+// @description     Translates websites with translation received from remote sources.
+// @description:tr  Uzaktaki kaynaklardan alinan tercumelerle web sitelerini cevirir.
+// @author          Omer Ozarslan <code@ozarslan.com>
+// @namespace       https://github.com/ozars/ceviriver
+// @downloadURL     https://raw.github.com/ozars/ceviriver/master/ceviriver.user.js
+// @license         Apache-2.0 https://www.apache.org/licenses/LICENSE-2.0
+// @copyright       Copyright (C) 2018, by Omer Ozarslan <code@ozarslan.com>
+//
+// @include         https://*.freecodecamp.org/*
+// @include         https://freecodecamp.org/*
+//
+// @require         https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @version         1.0
+// ==/UserScript==
+
+var main = function()
+{
+    console.log("It works!");
+}
+
+$(main);
+
